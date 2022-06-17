55f23fb0ca32385f956e9b822bcd84440be0fd3b
Vrag404 <vrag404@protonmail.com>
2022-06-17 18:40:29 -0300

    form correction

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,16 +1,43 @@
 import { useEffect, useState } from 'react'
+import { send } from '@emailjs/browser';
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const [toSend, setToSend] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 3000)
     })
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      send(
+        'service_fuq6zde',
+        'template_0bgt9lm',
+        toSend,
+        '1GbI42GEViqQ-XirX'
+      )
+        .then((response) => {
+          alert('Mensagem enviada!', response.status, response.text);
+        })
+        .catch((err) => {
+          alert('Erro! Mensagem não enviada.', err);
+        });
+    };
+
+    const handleChange = (e) => {
+      setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
 
   return (
     <>
@@ -29,22 +56,27 @@ const Contact = () => {
             você pode usar o formulário abaixo. 
           </p>
           <div className="contact-form">
-            <form action="https://getform.io/f/e4d33bbb-e030-466d-afe7-58ea0c890fac" method='POST'>
+            <form onSubmit={onSubmit}>
               <ul>
                 <li className='half'>
-                  <input type='text' name='name' placeholder='Nome' required />
+                  <input type='text' name='name' placeholder='Nome' 
+                    value={toSend.name} onChange={handleChange} required  />
                 </li>
 
                 <li className='half'>
-                  <input type='email' name='email' placeholder='Email' required />
+                  <input type='email' name='email' placeholder='Email' 
+                    value={toSend.email} onChange={handleChange} required />
                 </li>
                 
                 <li>
-                  <input placeholder='Assunto' type='text' name='subject' required />
+                  <input placeholder='Assunto' type='text' name='subject' 
+                    value={toSend.subject} onChange={handleChange} required />
                 </li>
 
                 <li>
-                  <textarea placeholder='Mensagem' name='message' required></textarea>
+                  <textarea placeholder='Mensagem' name='message' 
+                    value={toSend.message} onChange={handleChange} required>
+                    </textarea>
                 </li>
 
                 <li>
