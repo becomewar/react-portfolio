50a280fd7282103f3d951ff03223bef462a10abf
Vrag404 <vrag404@protonmail.com>
2022-06-29 21:15:11 -0300

    correction

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
+import React from 'react';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+          <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/skills' element={<Skills />} />
+          </Route>
+        </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
