fdd47ba43e4bdd823a0f242debe353a6e5fe8837
Vrag404 <vrag404@protonmail.com>
2023-02-03 04:17:50 -0300

    Creating object responsible for managing the links.

---
+++ src/components/Header/static/data.ts
@@ -0,0 +1,40 @@
+import { NavbarDataTypes } from "../models";
+
+export const NavbarData: NavbarDataTypes[] = [
+  {
+    id: 1,
+    item: "Home",
+    icon: "uil uil-home",
+    href: "home",
+  },
+  {
+    id: 2,
+    item: "About",
+    icon: "uil uil-user",
+    href: "about",
+  },
+  {
+    id: 3,
+    item: "Skills",
+    icon: "uil uil-file-alt",
+    href: "skills",
+  },
+  {
+    id: 4,
+    item: "Qualifications",
+    icon: "uil uil-graduation-cap",
+    href: "qualification",
+  },
+  {
+    id: 5,
+    item: "Projects",
+    icon: "uil uil-briefcase-alt",
+    href: "projects",
+  },
+  {
+    id: 6,
+    item: "Contact",
+    icon: "uil uil-envelope",
+    href: "contact",
+  },
+];
