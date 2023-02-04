6a72ff23d794afd337604a7b04bad28c749019f8
Vrag404 <vrag404@protonmail.com>
2023-02-04 09:57:19 -0300

    Configuring alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      exportAsDefault: true,
-    }),
-  ],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  plugins: [react()],
 });
