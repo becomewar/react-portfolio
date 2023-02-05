3c5d5219d26694e0a26db33637d5f9d51804dacd
Vrag404 <vrag404@protonmail.com>
2023-02-05 09:18:54 -0300

    Creating contact session

---
+++ src/components/Contact/ContactCard/index.tsx
@@ -0,0 +1,63 @@
+import { Slide } from "react-awesome-reveal";
+import "./styles.scss";
+
+export default function ContactCard() {
+  return (
+    <Slide direction="left" triggerOnce>
+      <div className="contact__content">
+        <h3 className="contact__title">Converse comigo</h3>
+
+        <div className="contact__info">
+          <div className="contact__card">
+            <i className="bx bx-mail-send conact__card-icon" />
+            <h3 className="conact__card-title">Email</h3>
+            <span className="contact__card-data">d.hcosta@proton.me</span>
+            <a
+              href="mailto: d.hcosta@proton.me"
+              target="_blank"
+              className="contact__button"
+              rel="noreferrer"
+            >
+              Entrar em contato
+              <i className="bx bx-right-arrow-alt contact__button-icon" />
+            </a>
+          </div>
+
+          <div className="contact__info">
+            <div className="contact__card">
+              <i className="bx bxl-whatsapp conact__card-icon" />
+              <h3 className="conact__card-title">Whatsapp</h3>
+              <span className="contact__card-data">+55 (19) 99974-1378</span>
+              <a
+                href="https://api.whatsapp.com/send?phone=3126900771&text=Hello, more information!"
+                target="_blank"
+                className="contact__button"
+                rel="noreferrer"
+              >
+                Entrar em contato
+                <i className="bx bx-right-arrow-alt contact__button-icon" />
+              </a>
+            </div>
+          </div>
+
+          <div className="contact__info">
+            <div className="contact__card">
+              <i className="bx bxl-discord conact__card-icon" />
+              <h3 className="conact__card-title">Discord</h3>
+              <span className="contact__card-data">becomewar#3029</span>
+              <a
+                href="https://discordapp.com/users/921538838709534772"
+                className="contact__button"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Entrar em contato
+                <i className="bx bx-right-arrow-alt contact__button-icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Slide>
+  );
+}
